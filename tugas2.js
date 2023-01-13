f9c5439a2c3afd0ad17c4f3d938efdd0ea8a959d
Ikhor03 <imamkhoiri725@gmail.com>
2023-01-13 13:38:04 +0700

    task 2

---
+++ tugas2.js
@@ -0,0 +1,8 @@
+let nama = "Ikhor";
+let umur = 24;
+let berkacamata = true;
+let alamat;
+let calendar = null;
+let friends = ["Nudew", "Kamil", "Randi", "Fahri"]
+
+console.log(nama, umur, berkacamata, alamat, calendar, friends);
